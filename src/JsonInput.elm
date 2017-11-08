c8e8b2a2b39f388086d73ad965ebf6d48215d1ad
Anatoliy <ac@ub.io>
2017-11-08 23:08:43 +0000

    Stringify json schema validation errors

--- src/JsonInput.elm
+++ src/JsonInput.elm
@@ -60,7 +60,7 @@ import Helpers
         )
 import JsonValue exposing (JsonValue(ObjectValue, ArrayValue))
 import Json.Schema as JS
-import Json.Schema.Validation as Validation exposing (Error)
+import Json.Schema.Validation as Validation exposing (Error, ValidationError(..))
 import Json.Schema.Definitions as Schema
     exposing
         ( Schema(BooleanSchema, ObjectSchema)
@@ -171,6 +171,108 @@ getValue model =
         |> JsonValue.encode
 
 
+pluralize : Int -> String -> String
+pluralize n name =
+    case n of
+        1 ->
+            "1 " ++ name
+
+        _ ->
+            toString n ++ " " ++ name ++ "s"
+
+
+stringifyError : Validation.ValidationError -> String
+stringifyError e =
+    case e of
+        MultipleOf multiplier actual ->
+            toString actual
+                ++ " is not a multiple of "
+                ++ (toString multiplier)
+
+        Maximum max actual ->
+            toString actual ++ " is more than " ++ (toString max)
+
+        Minimum min actual ->
+            toString actual ++ " is less than " ++ (toString min)
+
+        ExclusiveMaximum max actual ->
+            toString actual ++ " is more than " ++ (toString max)
+
+        ExclusiveMinimum min actual ->
+            toString actual ++ " is less than " ++ (toString min)
+
+        MaxLength expected actual ->
+            "Expected string not longer than "
+                ++ (pluralize expected "character")
+                ++ " but actual length is "
+                ++ (pluralize actual "character")
+
+        MinLength expected actual ->
+            "Expected string not shorter than "
+                ++ (pluralize expected "character")
+                ++ " but actual length is "
+                ++ (pluralize actual "character")
+
+        Pattern pattern string ->
+            "String " ++ (toString string) ++ " does not match pattern " ++ pattern
+
+        MaxItems expected actual ->
+            "List expected to have at most "
+                ++ (pluralize expected "item")
+                ++ " but it has "
+                ++ (pluralize actual "item")
+
+        MinItems expected actual ->
+            "List expected to have at least "
+                ++ (pluralize expected "item")
+                ++ " but it has "
+                ++ (pluralize actual "item")
+
+        UniqueItems x ->
+            "Expected array of unique items, but a duplicate found: " ++ (Encode.encode 0 x)
+
+        Contains ->
+            "None of array items is valid against the given schema"
+
+        MaxProperties expected actual ->
+            ""
+
+        MinProperties expected actual ->
+            ""
+
+        Required missingPropertyNames ->
+            ""
+
+        AdditionalPropertiesDisallowed extraPropertyNames ->
+            "Additional properties are not allowed here, but I see "
+                ++ (extraPropertyNames |> List.map toString |> String.join ", ")
+
+        InvalidPropertyName invalidPropertyNames ->
+            "Some property names are not passing validation: "
+                ++ (invalidPropertyNames |> List.map toString |> String.join ", ")
+
+        Enum ->
+            "Value does not match enumeration defined in the schema"
+
+        Const ->
+            "Value does not match const defined in the schema"
+
+        InvalidType s ->
+            s
+
+        OneOfNoneSucceed ->
+            "Value does not pass the validation with none of the schemata listed in '.oneOf'"
+
+        OneOfManySucceed int ->
+            "Value should pass validation with exactly one schema, but " ++ (toString int) ++ " return a positive result"
+
+        Not ->
+            "This value expected to fail validation"
+
+        AlwaysFail ->
+            "This is not expected to succeed"
+
+
 updateValue : Model -> String -> Result String EditableJsonValue -> ( Model, ExternalMsg )
 updateValue model path newStuff =
     let
@@ -181,10 +283,10 @@ updateValue model path newStuff =
                         (\x ->
                             case x of
                                 Just list ->
-                                    (toString err.error) :: list |> Just
+                                    (stringifyError err.error) :: list |> Just
 
                                 Nothing ->
-                                    [ err.error |> toString ] |> Just
+                                    [ err.error |> stringifyError ] |> Just
                         )
                 )
                 Dict.empty
