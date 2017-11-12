e015617c5ea4f477df856c273a6c2fc5f6f822e7
Anatoliy <ac@ub.io>
2017-11-12 18:16:16 +0000

    Fix indentation

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ echo ""
 echo "Pruning and minifying code..."
 
 uglifyjs \
-       	$DEST/elm.readable.js \
-       	-mc 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9",pure_getters=true,keep_fargs=false,unsafe_comps=true' \
-	2> /dev/null \
-	1> $DEST/elm.min.js
+  $DEST/elm.readable.js \
+  -mc 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9",pure_getters=true,keep_fargs=false,unsafe_comps=true' \
+  2> /dev/null \
+  1> $DEST/elm.min.js
 
 echo "Minified build size:"
 wc -c $DEST/elm.min.js
